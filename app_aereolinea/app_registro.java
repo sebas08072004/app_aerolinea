bf475870a24ae380a86cf6d7988b05e04859df30
sebastian chica <136635452+sebas08072004@users.noreply.github.com>
2024-03-07 01:09:27 -0500

    commit final

--- app_aereolinea/app_registro.java
+++ app_aereolinea/app_registro.java
@@ -29,58 +29,95 @@ public class app_registro {
                     int membresia =Integer.parseInt( JOptionPane.showInputDialog(null,"-------MENBRESIAS---------- \n 1. bronce: \n 2. plata: \n 3. oro: \n 4.platino: "));
                     
                     if(id.equals("") || nombre.equals("")|| contacto.equals("")||correo.equals("")||sexo.equals("")){
-                        JOptionPane.showMessageDialog(null,"debes ingresar todos los datos para poder realizar el registro ");
-                   
-                     }
-                    else{
-                    pasajero.add (new cls_registro(id, nombre, contacto, correo, sexo, membresia));
-                    JOptionPane.showMessageDialog(null, "has sido registrado con exito");
+                        JOptionPane.showMessageDialog(null, "Debes ingresar todos los datos para poder realizar el registro");
+                    } else {
+                        boolean encontrado = false;
+                        for (int i = 0; i < pasajero.size(); i++) {
+                            if (pasajero.get(i).getIdStr().equals(id)) {
+                                encontrado = true;
+                                break;
+                            }
+                        }
+            
+                        if (encontrado) {
+                            JOptionPane.showMessageDialog(null, "Ya te encuentrabas registrado, puedes hacer compras en este momento si deseas");
+                        } else {
+                            pasajero.add(new cls_registro(id, nombre, contacto, correo, sexo, membresia));
+                            JOptionPane.showMessageDialog(null, "Has sido registrado con éxito");
+                        }
                     }
                 }    
 
                 if ((men.equals("2"))) {
                     if(pasajero.size() > 0){
                         String id_cliente = JOptionPane.showInputDialog(null,"identificacion: ");
-
-                        float descu_membreFlt = 0;
-
-                        for(int i = 0; i < pasajero.size(); i++){
-                            if(pasajero.get(i).getIdStr().equals(id_cliente)){
-
-                                descu_membreFlt = pasajero.get(i).getDesc_membresiaFlt();
-                            
+                        boolean encontrado = false;
+                        for (int i = 0; i < pasajero.size(); i++) {
+                            if (pasajero.get(i).getIdStr().equals(id_cliente)) {
+                                encontrado = true;
+                                break;
                             }
                         }
-                        
-                        
-                        int cantidad = Integer.parseInt( JOptionPane.showInputDialog(null,"cantidad: "));
-                        String  tipo_clase =  JOptionPane.showInputDialog(null, "---------CLASE--------- \n1 economica: \n 2. turista \n 3. VIP \n");
+            
+                        if (encontrado) {
+                           
 
-                        String metodo_pago = JOptionPane.showInputDialog(null, "------METODO DE PAGO---------\n 1. efectivo (5%) \n 2. tarjeta credito (7%) \n 3. tarjeta ahorro (2%) \n4. transferencia (10%) \n");
-
-                        tiquetes.add(new cls_compra_tiquetes(id_cliente, cantidad, tipo_clase, metodo_pago, descu_membreFlt));
-                    }else{
-                        
-                    }
+                            float descu_membreFlt = 0;
+    
+                            for(int i = 0; i < pasajero.size(); i++){
+                                if(pasajero.get(i).getIdStr().equals(id_cliente)){
+    
+                                    descu_membreFlt = pasajero.get(i).getDesc_membresiaFlt();
+                                
+                                }
+                            }
+                            
+                            
+                            int cantidad = Integer.parseInt( JOptionPane.showInputDialog(null,"cantidad: "));
+                            String  tipo_clase =  JOptionPane.showInputDialog(null, "---------CLASE--------- \n1 economica: \n 2. turista \n 3. VIP \n");
+    
+                            String metodo_pago = JOptionPane.showInputDialog(null, "------METODO DE PAGO---------\n 1. efectivo (5%) \n 2. tarjeta credito (7%) \n 3. tarjeta ahorro (2%) \n4. transferencia (10%) \n");
+    
+                            tiquetes.add(new cls_compra_tiquetes(id_cliente, cantidad, tipo_clase, metodo_pago, descu_membreFlt));
+                        }else{
+                            JOptionPane.showMessageDialog(null,"no existen registros con este id, debes resgitrarte previamente para poder realizar una compra");
+                        }
+                        } else {
+                            
+                            JOptionPane.showMessageDialog(null, "no te encuentras registrado aun");
+                        }
+                       
                 }
                 if(men.equals("3")){
-                    String id_reporte = JOptionPane.showInputDialog(null, "identificacion: ");
-                    
-                    for(int i = 0; i < tiquetes.size(); i++) {
-                        if(tiquetes.get(i).getIdcliente().equals(id_reporte)){
-                             JOptionPane.showMessageDialog(null,"---------REPORTE---------- \n\nidentificacion: "+tiquetes.get(i).getIdcliente()+" \n nombre:  " + pasajero.get(i).getNombreStr()+ "\ncontacto: "+pasajero.get(i).getContactoStr()  + "\n cantidad: "+ tiquetes.get(i).getCantidadInt() + "\n subototal: "+ tiquetes.get(i).getSubtotalFlt() + "\n descuento: "+ tiquetes.get(i).getDescuentoFlt()+"\niva: "+ tiquetes.get(i).getIvaFlt()+ "\n total: "+ tiquetes.get(i).getTotalFlt());
+                    if(pasajero.size()>0){
+
+                        if(tiquetes.size() > 0){
+                            String id_reporte = JOptionPane.showInputDialog(null, "identificacion: ");
+                            
+                            for(int i = 0; i < tiquetes.size(); i++) {
+                                if(tiquetes.get(i).getIdcliente().equals(id_reporte)){
+                                    JOptionPane.showMessageDialog(null,"---------REPORTE---------- \n\nidentificacion: "+tiquetes.get(i).getIdcliente()+" \n nombre:  " + pasajero.get(i).getNombreStr()+ "\ncontacto: "+pasajero.get(i).getContactoStr()  + "\n cantidad: "+ tiquetes.get(i).getCantidadInt() + "\n subototal: "+ tiquetes.get(i).getSubtotalFlt() + "\n descuento: "+ tiquetes.get(i).getDescuentoFlt()+"\niva: "+ tiquetes.get(i).getIvaFlt()+ "\n total: "+ tiquetes.get(i).getTotalFlt());
+                                }
+
+                            }
+                        }else{
+                            JOptionPane.showMessageDialog(null, "no existen compras resgistradas, realiza una compra para poder ver el reporte");
                         }
 
                     }
 
-                }
-                if(men.equals("4")){
-                    System.exit(0);
-                }
-            
-            
-            }
+                    else{
+                        JOptionPane.showMessageDialog(null, "no existen resgistros, debes registrarte para poder realizar una compra y posteriormente ver el reporte");
+                    }
+                    
+                 }
+
+                 if(men.equals("4")){
+                    m = false;
+                    
+                 }
                 
+            }
+        }    
     }
-}
 }
